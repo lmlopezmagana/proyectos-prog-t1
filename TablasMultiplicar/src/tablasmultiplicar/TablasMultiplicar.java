ceb437aadfaaef48d5d050018ef278ade27d8e5f
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2023-01-13 13:31:26 +0100

    Que me gusta en el camino un Land Rover lleno de gambas

--- TablasMultiplicar/src/tablasmultiplicar/TablasMultiplicar.java
+++ TablasMultiplicar/src/tablasmultiplicar/TablasMultiplicar.java
@@ -5,7 +5,7 @@ public class TablasMultiplicar {
 	public static void main(String[] args) {
 
 		//TODO Terminar este método
-		for (int i = 1; i <= 10; i++) {
+		for (int i = 1; i <= 11; i++) {
 			System.out.println("Tabla del " + i);
 			for (int j = 1; j <= 10; j++) {
 				System.out.printf("%d*%d = %d\n", i, j, i * j);
